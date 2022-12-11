1ab7ee0a90c694f742ae21b6eff13dd7f3de61ae
Sagar <sagarmohanty5509@gmail.com>
2022-12-11 14:48:35 +0530

    bug fix

--- client/src/Components/BookModal.js
+++ client/src/Components/BookModal.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Modal, Button, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBookAtlas,
+  faBookOpen,
+  faUserTie,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function BookModal(props) {
   // let image = props?.data?.image?.image;
@@ -18,12 +24,18 @@ export default function BookModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
+          <FontAwesomeIcon icon={faBookOpen} />
+          {"  "}
           {firstCaps(props?.data?.title)}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Col md={9}>
-          <h4>Author: {firstCaps(props?.data?.author)}</h4>
+          <h4>
+            <FontAwesomeIcon icon={faUserTie} />
+            {"  "}
+            Author: {firstCaps(props?.data?.author)}
+          </h4>
           <p> {props?.data?.subject}</p>
         </Col>
       </Modal.Body>
--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://sore-rose-pike-sari.cyclic.app",
 });
 
 export const postBook = (payload) => api.post("/api/addBooks", payload);
